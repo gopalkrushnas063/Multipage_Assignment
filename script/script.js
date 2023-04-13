b8dec813ce43ff1744cfdef43c9dd7c836b29bf8
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-04-13 19:15:27 +0530

    File Upload Script Updated

--- script/script.js
+++ script/script.js
@@ -10,6 +10,11 @@ function prevStep(step) {
   document.getElementById("step" + (step - 1)).style.display = "block";
 }
 
+
+
+
+
+
 // Constructor For Form Data
 function ApplicantDetails(
   firstName,
@@ -97,10 +102,10 @@ function storeData(event) {
   var currentInstitution = document.getElementById("currentInstitution").value;
   var overallScore = document.getElementById("overallScore").value;
   var backlogs = document.getElementById("backlogs").value;
-  // var photo = document.getElementById("photo").value;
-  // var hscMarksheet = document.getElementById("hscMarksheet").value;
-  // var sscMarksheet = document.getElementById("sscMarksheet").value;
-  // var allMarksheets = document.getElementById("allMarksheets").value;
+  var photo = document.getElementById("photo").value;
+  var hscMarksheet = document.getElementById("hscMarksheet").value;
+  var sscMarksheet = document.getElementById("sscMarksheet").value;
+  var allMarksheets = document.getElementById("allMarksheets").value;
 
   // Perform validation
   if (
@@ -130,77 +135,45 @@ function storeData(event) {
   ) {
     alert("Please fill in all fields");
   }
-  // Read photo file as Data URL
-  var photoFile = document.getElementById("photo").files[0];
-  var photoReader = new FileReader();
-  photoReader.onloadend = function () {
-    photo = photoReader.result;
 
-    // Read hscMarksheet file as Data URL
-    var hscMarksheetFile = document.getElementById("hscMarksheet").files[0];
-    var hscMarksheetReader = new FileReader();
-    hscMarksheetReader.onloadend = function () {
-      hscMarksheet = hscMarksheetReader.result;
+  let formData = new ApplicantDetails(
+    firstName,
+    lastName,
+    dob,
+    email,
+    fatherFirstName,
+    fatherLastName,
+    motherFirstName,
+    motherLastName,
+    nationality,
+    streetAddress,
+    city,
+    country,
+    mobile,
+    hscInstitution,
+    hscBoard,
+    hscScore,
+    sscInstitution,
+    sscBoard,
+    sscScore,
+    currentlyPursuing,
+    currentInstitution,
+    overallScore,
+    backlogs,
+    photo,
+    hscMarksheet,
+    sscMarksheet,
+    allMarksheets
+  );
 
-      // Read sscMarksheet file as Data URL
-      var sscMarksheetFile = document.getElementById("sscMarksheet").files[0];
-      var sscMarksheetReader = new FileReader();
-      sscMarksheetReader.onloadend = function () {
-        sscMarksheet = sscMarksheetReader.result;
+  let formDataLS = JSON.parse(localStorage.getItem("applicant")) || [];
 
-        // Read allMarksheets file as Data URL
-        var allMarksheetsFile =
-          document.getElementById("allMarksheets").files[0];
-        var allMarksheetsReader = new FileReader();
-        allMarksheetsReader.onloadend = function () {
-          allMarksheets = allMarksheetsReader.result;
+  // Store form data in localStorage
+  formDataLS.push(formData);
 
-          let formData = new ApplicantDetails(
-            firstName,
-            lastName,
-            dob,
-            email,
-            fatherFirstName,
-            fatherLastName,
-            motherFirstName,
-            motherLastName,
-            nationality,
-            streetAddress,
-            city,
-            country,
-            mobile,
-            hscInstitution,
-            hscBoard,
-            hscScore,
-            sscInstitution,
-            sscBoard,
-            sscScore,
-            currentlyPursuing,
-            currentInstitution,
-            overallScore,
-            backlogs,
-            photo,
-            hscMarksheet,
-            sscMarksheet,
-            allMarksheets
-          );
+  // Convert form data to JSON string && Store form data in localStorage with a key
+  localStorage.setItem("applicant", JSON.stringify(formDataLS));
 
-          let formDataLS = JSON.parse(localStorage.getItem("applicant")) || [];
-
-          // Store form data in localStorage
-          formDataLS.push(formData);
-
-          // Convert form data to JSON string && Store form data in localStorage with a key
-          localStorage.setItem("applicant", JSON.stringify(formDataLS));
-
-          alert("Registration Successfully Done !");
-          window.location.reload();
-        };
-        allMarksheetsReader.readAsDataURL(allMarksheetsFile);
-      };
-      sscMarksheetReader.readAsDataURL(sscMarksheetFile);
-    };
-    hscMarksheetReader.readAsDataURL(hscMarksheetFile);
-  };
-  photoReader.readAsDataURL(photoFile);
+  alert("Registration Successfuly Done !");
+  window.location.reload();
 }
